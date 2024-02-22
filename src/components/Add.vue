06ff9cde5cf86860f00513abe00612a290314a81
David wanjohi <davidwagura83@gmail.com>
2024-02-22 09:40:08 +0300

    add a create form

---
+++ src/components/Add.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Add New Item</h2>
+        <form @submit.prevent="addItem">
+            <label for="itemName">ItemName:</label>
+            <input type="text" id="itemName" v-model="newItemName" required />
+            <button type="submit">Add Item</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default { 
+    data() {
+        return {
+            newItemName: '',
+        };
+    },
+    methods: {
+        addItem() {
+            //implement add functionality
+        },
+    },
+};
+</script>
