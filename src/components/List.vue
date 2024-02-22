b929079db9d419b2172ccdd5ab6664df2add2506
David wanjohi <davidwagura83@gmail.com>
2024-02-22 09:29:06 +0300

    create a List.vue

---
+++ src/components/List.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h2>All Items</h2>
+        <ul>
+            <li v-for="item in items" :key="item.id">
+                {{ item.name }}
+                <button @click="editItem(item)">Edit</button>
+                <button @click="deleteItem(item.id)">Delete</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                items: [],
+            };
+        },
+        methods: {
+            editItem(item) {
+                //implement edit functionality
+            },
+            deleteItem(item) {
+                //implement delete functionality
+            },
+        },
+        mounted() {
+            //fetch items from the serveror an API
+            //add assign them to the 'items' data property
+        },
+    };
+</script>
