9f4feea836ac22cf5a5142b6d6314eef5d237cc3
David wanjohi <davidwagura83@gmail.com>
2024-02-22 09:54:05 +0300

    add Edit.vue

---
+++ src/components/Edit.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Edit Item</h2>
+        <form @submit.prevent="updateItem">
+            <label for="editItemName">Item Name:</label>
+            <input type="text" id="editedItemName" v-model="editedItemName" required>
+            <button type="submit">update Item</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            editedItemName: '',
+        };
+    },
+    methods: {
+        updateItem() {
+            //implement update functionality
+        },
+    },
+};
+</script>
